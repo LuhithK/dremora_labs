c97fcbc8c70ceee5dbe712688013b9589cd24cf5
LuhithK <121594327+LuhithK@users.noreply.github.com>
2025-09-21 01:20:31 +0530

    Fix tailwind.config.js syntax error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -37,5 +39,5 @@ export default {
       },
     },
   },
-  plugins: [@tailwindcss/forms],
+  plugins: [forms],
 }
